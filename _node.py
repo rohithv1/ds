0ecbca135dba43819989046870d60983086efc75
Rohith Vemulapally <36758930+rohithv1@users.noreply.github.com>
2020-09-29 21:53:58 +0530

    Add files via upload

---
+++ _node.py
@@ -0,0 +1,19 @@
+"""
+Node class for creating various Datastructures
+
+@author: Rohith
+"""
+
+class Node(object):
+    """
+    Node class with curr and nexr values
+    """
+
+    def __init__(self, curr, next):
+        """
+        Args:
+            curr: current value the node is handling
+            next: next value
+        """
+        self.curr = curr
+        self.next = next
